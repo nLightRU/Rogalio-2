bcd2b3291eed171896103dd83d6c830fdc29408a
Ilya <northenlight6899@gmail.com>
2024-02-16 21:40:52 +0300

    move main

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,56 @@
 #include <memory>
 #include <utility>
 #include <vector>
+#include <ctime>
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
-
-// start 384, 400
-// end 447, 447
+// start 735, 207
+// size 20 x 20
 
 
 int main()
 {
+    std::srand(std::time(nullptr));
+
     sf::RenderWindow window(sf::VideoMode(800, 600), 
                            "Rogalio 2", 
                            sf::Style::Resize | sf::Style::Titlebar | sf::Style::Close);
 
     
-    
-    sf::Texture floor_texture;
-    if ( !floor_texture.loadFromFile("level.png", sf::IntRect(736, 208, 40, 40)) )
+    sf::Image* asset_map = new sf::Image();
+    if (!asset_map->loadFromFile(R"(res\RF_Catacombs_V1.0\mainlevbuild.png)"))
     {
-        std::cerr << "ERROR in loading texture from file" << std::endl;
-        exit(4);
+        exit(3);
+    }
+    
+    std::vector<sf::Texture> floor_textures;
+    floor_textures.reserve(6);
+
+    for (size_t i = 0; i < 3; ++i)
+    {
+        for(size_t j = 0; j < 2; ++j)
+        {
+            sf::Texture t;
+            t.loadFromImage(*asset_map, sf::IntRect(735 + j*16, 207 + i*16, 16, 16));
+            floor_textures.emplace_back(std::move(t));
+        }
     }
 
-    sf::Vector2f initial_position{40.f, 40.f};
-    sf::Sprite sprite(floor_texture);
-    sprite.setPosition(initial_position);
 
+    sf::Vector2f start_pos = sf::Vector2f(80.f, 80.f);
+    std::vector<sf::Sprite> sprites;
+    for (size_t i = 0; i < 15; ++i)
+    {
+        for (size_t j = 0; j < 15; ++j)
+        {
+            int idx = std::rand() % 6;
+            sf::Sprite s(floor_textures[idx]);
+            sf::Vector2f pos = start_pos + sf::Vector2f(16.f * i, 16.f * j);
+            s.setPosition(pos);
+            sprites.push_back(std::move(s));
+        }
+    }
+            
     while (window.isOpen())
     {
         sf::Event event;
@@ -38,13 +61,18 @@ int main()
                 window.close();
         }
 
-        window.clear(sf::Color::White);
+        window.clear(sf::Color::Black);
 
-        window.draw(sprite);
+        for (const auto& s : sprites) 
+        {
+            window.draw(s);
+        }
+        
         
         window.display();
     }
 
+    delete asset_map;
  
     return 0;
 }
